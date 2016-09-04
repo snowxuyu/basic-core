05743870274711d0e824dccd8e55c443193e2d5e
gaoguoxiang <snowxuyu@gmail.com>
2016-09-05 01:04:25 +0800

    mybatis版本更新BaseDao @Options flushCache=false 修改为 flushCache=Options.FlushCachePolicy.FALSE   true也做了相应的修改

--- src/main/java/org/framework/basic/dao/BaseDao.java
+++ src/main/java/org/framework/basic/dao/BaseDao.java
@@ -12,37 +12,37 @@ import java.util.List;
 
 public abstract interface BaseDao<T extends BaseEntity> {
     @SelectProvider(type = MyBatisProvider.class, method = "getAll")
-    @Options(flushCache = false,useCache = true)
+    @Options(flushCache = Options.FlushCachePolicy.FALSE,useCache = true)
     @ResultMap("getMap")
     public List<T> getAll();
 
     @SelectProvider(type = MyBatisProvider.class, method = "getById")
-    @Options(flushCache = false,useCache = true)
+    @Options(flushCache = Options.FlushCachePolicy.FALSE,useCache = true)
     @ResultMap("getMap")
     public T getById(String id);
 
     @InsertProvider(type = MyBatisProvider.class, method = "insert")
-    @Options(keyProperty = "id",flushCache = true)
+    @Options(keyProperty = "id",flushCache = Options.FlushCachePolicy.TRUE)
     public int insert(T t);
 
     @InsertProvider(type = MyBatisProvider.class, method = "insertBatch")
-    @Options(keyProperty = "id",flushCache = true)
+    @Options(keyProperty = "id",flushCache = Options.FlushCachePolicy.TRUE)
     public int insertBatch(@Param("list")List<T> list);
 
     @UpdateProvider(type = MyBatisProvider.class, method = "update")
-    @Options(flushCache = true)
+    @Options(flushCache = Options.FlushCachePolicy.TRUE)
     public int update(T t);
 
     @DeleteProvider(type = MyBatisProvider.class, method = "deleteById")
-    @Options(flushCache = true)
+    @Options(flushCache = Options.FlushCachePolicy.TRUE)
     public int deleteById(String id);
 
     @DeleteProvider(type = MyBatisProvider.class, method = "deleteByPrimaryKey")
-    @Options(flushCache = true)
+    @Options(flushCache = Options.FlushCachePolicy.TRUE)
     public int deleteByPrimaryKey(T t);
 
     @DeleteProvider(type = MyBatisProvider.class,method = "deleteAll")
-    @Options(flushCache = true)
+    @Options(flushCache = Options.FlushCachePolicy.TRUE)
     public int deleteAll();
 
 }
